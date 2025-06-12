b1db55ac32eac9371d33291ae216cebe1ab73d08
Kiyan6787 <kiyanramjanum94@gmail.com>
2025-06-12 12:57:48 +0200

    "Updated to vercel style"

--- api/sort.js
+++ api/sort.js
@@ -1,20 +1,17 @@
-// api/sort.js
-const express = require('express');
-const serverless = require('serverless-http');
+// File: api/sort.js
 
-const app = express();
-app.use(express.json());
-
-app.post('/', (req, res) => {
-  const { data } = req.body;
-
-  if (typeof data !== 'string') {
-    return res.status(400).json({ error: '"data" must be a string' });
+export default function handler(req, res) {
+    if (req.method !== 'POST') {
+      return res.status(405).json({ error: 'Method not allowed' });
+    }
+  
+    const { data } = req.body;
+  
+    if (typeof data !== 'string') {
+      return res.status(400).json({ error: '"data" must be a string' });
+    }
+  
+    const sorted = data.split('').sort();
+    return res.status(200).json({ word: sorted });
   }
-
-  const sorted = data.split('').sort();
-  res.json({ word: sorted });
-});
-
-module.exports = app;
-module.exports.handler = serverless(app); // THIS is required by Vercel
+  
