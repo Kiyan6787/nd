0a9f8fe575c5c36986892774f36dbd56bae7b7d0
Kiyan6787 <kiyanramjanum94@gmail.com>
2025-06-12 12:54:58 +0200

    "Updated app.js"

--- api/sort.js
+++ api/sort.js
@@ -1,18 +1,20 @@
+// api/sort.js
 const express = require('express');
 const serverless = require('serverless-http');
 
 const app = express();
 app.use(express.json());
 
-app.post('/sort', (req, res) => {
+app.post('/', (req, res) => {
   const { data } = req.body;
 
   if (typeof data !== 'string') {
-    return res.status(400).json({ error: '"data" must be a string.' });
+    return res.status(400).json({ error: '"data" must be a string' });
   }
 
-  const sortedChars = data.split('').sort();
-  res.status(200).json({ word: sortedChars });
+  const sorted = data.split('').sort();
+  res.json({ word: sorted });
 });
 
-module.exports.handler = serverless(app);
+module.exports = app;
+module.exports.handler = serverless(app); // THIS is required by Vercel
